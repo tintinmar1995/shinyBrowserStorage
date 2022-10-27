d0ca3ba74f14cc89823188c33ef2566246bf62bb
Martin M <martin.masson@datastorm.fr>
2022-10-27 14:58:23 +0200

    DRAFT: Init R6 classes

--- R/localstorage.R
+++ R/localstorage.R
@@ -1,1 +1,18 @@
 # R6 class
+
+# LocalStorage <- R6Class(
+#   "LocalStorage",
+#   private = list(
+#     ...
+#   ),
+#   public = list(
+#     initialize = function() {
+#       ...
+#     },
+#     log_input = function(
+#       ...
+#     ) {
+#       ...
+#     }
+#   )
+# )
--- R/sessionstorage.R
+++ R/sessionstorage.R
@@ -1,1 +1,18 @@
 # R6 class
+
+# SessionStorage <- R6Class(
+#   "SessionStorage",
+#   private = list(
+#     ...
+#   ),
+#   public = list(
+#     initialize = function() {
+#       ...
+#     },
+#     log_input = function(
+#       ...
+#     ) {
+#       ...
+#     }
+#   )
+# )
